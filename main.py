aeb8043a9d43739ae6e1cc66f0332588b0457547
xerorex <kuba.arihant@gmail.com>
2022-01-26 21:02:01 +0530

    Updated comments

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-# This is a sample Python script.
+# This is a test Discord Bot.
+# This is not for commercial use.
+# Use this bot/code at your own risk
 import os
 import discord
 from dotenv import load_dotenv
 
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def main():
+    # get values from the .evn file
     load_dotenv()
     TOKEN = os.getenv('DISCORD_TOKEN')
     SUPER = os.getenv('SUPER_ID')
 
     client = discord.Client()
 
+    # Ready up the bot
     @client.event
     async def on_ready():
         print(f'{client.user.name}, Hello, hello friend!')
 
+    # Start listing to commands
     @client.event
     async def on_message(message):
         if message.author == client.user:
@@ -30,11 +30,12 @@ def main():
             else:
                 await message.channel.send('Hi!')
         elif message.content.startswith('!weather'):
+            print(message.content[9:])
             await message.channel.send('This feature is under development')
 
     client.run(TOKEN)
 
 
-# Press the green button in the gutter to run the script.
+# Run main only if this is run as a program and not if this file is called
 if __name__ == '__main__':
     main()
