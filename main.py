f4626e4dbe4d9c0950e0193daf5e3718a8ddce35
xerorex <kuba.arihant@gmail.com>
2022-01-26 20:47:09 +0530

    Added an if condition (and other requirements) to respond to a superuser differently Also created a placeholder for a !weather command

--- main.py
+++ main.py
@@ -11,12 +11,26 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
     TOKEN = os.getenv('DISCORD_TOKEN')
+    SUPER = os.getenv('SUPER_ID')
 
     client = discord.Client()
 
     @client.event
     async def on_ready():
         print(f'{client.user.name}, Hello, hello friend!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        if message.content.startswith('!hello'):
+            if message.author.id == int(SUPER):
+                await message.channel.send('All hail!')
+            else:
+                await message.channel.send('Hi!')
+        elif message.content.startswith('!weather'):
+            await message.channel.send('This feature is under development')
 
     client.run(TOKEN)
 
